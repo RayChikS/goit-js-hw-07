f7f34b8b05b359c2f888446a053c06d67cc99f15
RayChikS <vatoresp@gmail.com>
2023-10-31 14:05:34 +0300

    first

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,13 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+//знаходимо всі елементи img тому юзаємо querySelectorAll
+const galleryImages = document.querySelectorAll('.gallery__image'); 
+
+//перебираємо всі елементи та змінюємо їх значення через forEach
+galleryImages.forEach((image, index) => {
+  image.dataset.source = galleryItems[index].original; 
+  image.src = galleryItems[index].preview; 
+  image.alt = galleryItems[index].description; 
+});
