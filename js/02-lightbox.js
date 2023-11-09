ea12288a00e5a32b6826554d134fafe35ffa687d
RayChikS <vatoresp@gmail.com>
2023-11-09 15:08:06 +0300

    Final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,7 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const galleryImages = document.querySelectorAll('.gallery__image'); 
+
 
 const gallery = document.querySelector('ul.gallery');
 
@@ -9,45 +9,30 @@ const gallery = document.querySelector('ul.gallery');
 
 
 function createGalleryItem(previewSrc, originalSrc, description) {
-  // Створення елемента <li> для галереї
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-
-  // Створення посилання <a>
-  const galleryLink = document.createElement('a');
-  galleryLink.classList.add('gallery__link');
-  galleryLink.href = originalSrc;
-
-  // Створення зображення <img>
-  const galleryImage = document.createElement('img');
-  galleryImage.classList.add('gallery__image');
-  galleryImage.src = previewSrc;
-  galleryImage.dataset.source = originalSrc;
-  galleryImage.alt = description;
-
-  // Додавання <img> до <a>
-  galleryLink.appendChild(galleryImage);
-
-  // Додавання <a> до <li>
-  galleryItem.appendChild(galleryLink);
-
-	//Повертаємо елемент <li>
-  return galleryItem;
+	return `
+	<li class="gallery__item">
+  <a class="gallery__link" href="${originalSrc}">
+    <img
+      class="gallery__image"
+      src="${previewSrc}"
+      data-source="${originalSrc}"
+      alt="${description}"
+    />
+  </a>
+</li>
+	`
 }
 
 //Створюємо ф-цію для завантаження зображень з масива
-function createGallery() {
-
-  galleryItems.forEach((item) => {
-    const galleryItem = createGalleryItem(item.preview, item.original, item.description);
-    gallery.appendChild(galleryItem);
-  });
+function createGallery(arr) {
+	const markup = arr.map(item => createGalleryItem(item.preview, item.original, item.description)).join('');
+	gallery.innerHTML = markup;
 }
 //Викликаємо ф-цію
-createGallery();
+createGallery(galleryItems);
 
 
-var lightbox = new SimpleLightbox('.gallery a', { 
+const lightbox = new SimpleLightbox('.gallery a', { 
 	captionsData: "alt",
   captionDelay: 250,
  });
